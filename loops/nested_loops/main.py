93f1a8695db493ba6ffe4dbf41625a29632d1727
melisik <>
2026-04-30 12:02:10 +0000

    Nested loops

---
+++ loops/nested_loops/main.py
@@ -0,0 +1,8 @@
+produce = ["Tomatoes", "Lettuce"]
+dairy = ["Milk", "Cheese"]
+groceries = []
+for item in produce, dairy:
+    groceries.append(item)
+
+for item in groceries:
+    print(str(item))
